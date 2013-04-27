bd23df903696953ef5943d809d8e66c22504534b
Vikash Dat <vikash.dat@controlgroup.com>
2013-04-27 19:35:27 -0400

    persisting state between jenkins sessions. Also now reading in git commit sha

--- product/src/main/java/com/controlgroup/ci/jenkinsgithubcommitstatus/GithubStatusNotification.java
+++ product/src/main/java/com/controlgroup/ci/jenkinsgithubcommitstatus/GithubStatusNotification.java
@@ -1,12 +1,21 @@
 package com.controlgroup.ci.jenkinsgithubcommitstatus;
 
+import hudson.EnvVars;
 import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
+import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Company: Control Group
@@ -18,7 +27,7 @@ import org.kohsuke.stapler.DataBoundConstructor;
  */
 public class GithubStatusNotification extends Notifier {
 
-    private String repositoryName, repositoryOwner;
+    private final String repoName, repoOwner;
 
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.BUILD;
@@ -26,17 +35,39 @@ public class GithubStatusNotification extends Notifier {
 
     @DataBoundConstructor
     public GithubStatusNotification(String repoName, String repoOwner){
-        repositoryName = repoName;
-        repositoryOwner = repoOwner;
+        this.repoName = repoName;
+        this.repoOwner = repoOwner;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+            throws InterruptedException, IOException {
+
+        //Get build environment variables such as git sha
+        EnvVars envVars = new EnvVars();
+        envVars = build.getEnvironment(listener);
+        listener.getLogger().println("Notifing github with status for sha: " + envVars.get("GIT_COMMIT"));
+
+        return true;    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public String getRepoOwner() {
+        return repoOwner;
     }
 
 
     /*
-     *
-     * Descriptor class needed by Jenkins to help identify and manage plugin
-     */
+    *
+    * Descriptor class needed by Jenkins to help identify and manage plugin
+    */
     @Extension
     public static class Descriptor extends BuildStepDescriptor<Publisher>{
+        private String repoName;
+        private String repoOwner;
 
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
@@ -47,6 +78,30 @@ public class GithubStatusNotification extends Notifier {
         @Override
         public String getDisplayName() {
             return "Github Repository Status Notification";  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            repoName = json.getString("repoName");
+            repoOwner = json.getString("repoOwner");
+            save();
+            return super.configure(req, json);
+        }
+
+        public String getRepoName() {
+            return repoName;
+        }
+
+        public void setRepoName(String repoName) {
+            this.repoName = repoName;
+        }
+
+        public String getRepoOwner() {
+            return repoOwner;
+        }
+
+        public void setRepoOwner(String repoOwner) {
+            this.repoOwner = repoOwner;
         }
     }
 }
